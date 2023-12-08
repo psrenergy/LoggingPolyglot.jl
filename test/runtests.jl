8dbdabb811b056f51985341cc85059d7f0d16b38
raphasampaio <rsampaio@psr-inc.com>
2023-12-08 13:51:07 -0300

    Updates

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,4 +1,15 @@
+import LoggingPolyglot
+
+using Aqua
 using Test
+
+function testall()
+@testset "Aqua.jl" begin
+    @testset "Ambiguities" begin
+        Aqua.test_ambiguities(LoggingPolyglot, recursive = false)
+    end
+    Aqua.test_all(LoggingPolyglot, ambiguities = false)
+end
 
 for file in readdir(@__DIR__)
     if file in ["runtests.jl"]
@@ -10,3 +21,6 @@ for file in readdir(@__DIR__)
         include(file)
     end
 end
+end
+
+testall()
