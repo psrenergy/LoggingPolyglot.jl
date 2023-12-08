74459dde9e848a2092e30a408bd6096718f244f0
raphasampaio <rsampaio@psr-inc.com>
2023-12-08 14:03:17 -0300

    Updates

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,20 +4,20 @@ using Aqua
 using Test
 
 function testall()
-@testset "Aqua.jl" begin
-    Aqua.test_all(LoggingPolyglot)
-end
+    @testset "Aqua.jl" begin
+        Aqua.test_all(LoggingPolyglot)
+    end
 
-for file in readdir(@__DIR__)
-    if file in ["runtests.jl"]
-        continue
-    elseif !endswith(file, ".jl")
-        continue
+    for file in readdir(@__DIR__)
+        if file in ["runtests.jl"]
+            continue
+        elseif !endswith(file, ".jl")
+            continue
+        end
+        @testset "$(file)" begin
+            include(file)
+        end
     end
-    @testset "$(file)" begin
-        include(file)
-    end
-end
 end
 
 testall()
